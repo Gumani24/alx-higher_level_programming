6416e4fafa0325403066b84d162eade62ab7c953
Gumani24 <nengudzaernesto@gmail.com>
2023-06-26 11:27:03 -0700

    safe function

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size, alloc, i;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
+
+	size = var->ob_size;
+	alloc = list->allocated;
+
+	fflush(stdout);
+
+	printf("[*] Python list info\n");
+	if (strcmp(p->ob_type->tp_name, "list") != 0)
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+}
